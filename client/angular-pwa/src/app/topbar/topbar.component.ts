10775160ca160788065f11e2310e32cf1a1dbc10
Tommi <n5008@student.jamk.figit config --global user.email n5008@student.jamk.figit config --global user.email n5008@student.jamk.fi>
2022-10-11 10:33:13 +0300

    added elements and features to students frontpage

--- client/angular-pwa/src/app/topbar/topbar.component.ts
+++ client/angular-pwa/src/app/topbar/topbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Location} from '@angular/common';
 
 @Component({
   selector: 'app-topbar',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TopbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private location: Location) { }
+
+  // goBack():void {
+  //   this.location.back();
+  // }
 
   ngOnInit(): void {
   }
