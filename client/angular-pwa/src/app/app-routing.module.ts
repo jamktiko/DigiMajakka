ba85ffd2cb7d4c55f3fcc053d23e776d63adee7b
Tommi <n5008@student.jamk.figit config --global user.email n5008@student.jamk.figit config --global user.email n5008@student.jamk.fi>
2022-10-11 11:03:49 +0300

    routing

--- client/angular-pwa/src/app/app-routing.module.ts
+++ client/angular-pwa/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 /* eslint-disable @typescript-eslint/consistent-type-imports */
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
+import { ProfileComponent } from './profile/profile.component';
 import { StudentFrontpageComponent } from './student-frontpage/student-frontpage.component';
 
 const routes: Routes = [
-	{path: 'student', component: StudentFrontpageComponent}
+	{path: 'student', component: StudentFrontpageComponent},
+	{path: 'profile', component: ProfileComponent},
 ];
 
 @NgModule({
