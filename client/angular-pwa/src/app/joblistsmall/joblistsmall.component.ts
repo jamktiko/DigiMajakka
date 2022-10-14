192d2a2d226de188346609e3850f360e4140a3fe
Tommi <n5008@student.jamk.figit config --global user.email n5008@student.jamk.figit config --global user.email n5008@student.jamk.fi>
2022-10-14 11:32:26 +0300

    juttuja

--- client/angular-pwa/src/app/joblistsmall/joblistsmall.component.ts
+++ client/angular-pwa/src/app/joblistsmall/joblistsmall.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { JobService } from '../job.service';
 
 @Component({
   selector: 'app-joblistsmall',
@@ -8,31 +9,20 @@ import { Component, OnInit } from '@angular/core';
 export class JoblistsmallComponent implements OnInit {
 
 
-  constructor() { }
+  constructor(private jobService: JobService) { }
 
   // jobs-array will contain all job-listing fetched from the database
   // REPLACE PLACEHOLDER DATA WITH REAL DATA
-  jobs = [{
-    id: 1, company: 'Oy yritys Ab', desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    title: 'Orja'
-  },
-  {
-    id: 2, company: 'Velehot oy', desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    title: 'Fronttivelho'
-  },
-  {
-    id: 3, company: 'AsdASasdfffassssdasdasd', desc: 'Lorem ipsum dolors asasdddddddddd daaaa aaaaa aaaaaaaaaaa aaaaa aaakdlamdöamöaaaaaaaaaaaaa',
-    title: 'ASKDL'
-  },
-  {
-    id: 4, company: 'Joku hieno yritys', desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    title: 'Tietokantaosaaja'
-  },
-  ];
+  jobs: any = [];
 
   
 
   ngOnInit(): void {
+    this.getJobs();
+  }
+
+  getJobs(): void {
+    this.jobService.getJobs().subscribe(jobs => this.jobs = jobs)
   }
 
 }
