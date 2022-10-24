3a6fe02dea34b3af70032c82713529eb8a6840bb
Tommi <n5008@student.jamk.figit config --global user.email n5008@student.jamk.figit config --global user.email n5008@student.jamk.fi>
2022-10-24 12:50:00 +0300

    henkilötietojen muokkauslomakkeen runko valmiiksi, lomakkeen validointeja alkuun

--- client/angular-pwa/src/app/edit-personal-info/edit-personal-info.component.ts
+++ client/angular-pwa/src/app/edit-personal-info/edit-personal-info.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-personal-info',
@@ -7,9 +8,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditPersonalInfoComponent implements OnInit {
 
+  firstname: string = '';
+  lastname: string = '';
+  field: string = '';
+  school: string = '';
+  city: string = '';
+
+  detailForm!: FormGroup;
+
+
   constructor() { }
 
   ngOnInit(): void {
+    this.detailForm = new FormGroup({
+      firstname: new FormControl(this.firstname, [
+        Validators.required,
+        Validators.minLength(2),
+        Validators.maxLength(45),
+      ]),
+      lastname: new FormControl(this.firstname, [
+        Validators.required,
+        Validators.minLength(2),
+        Validators.maxLength(45),
+      ]),
+      field: new FormControl(this.firstname, [
+        Validators.required,
+      ]),
+      school: new FormControl(this.school, [
+        Validators.required,
+      ]),
+      city: new FormControl(this.city, [
+        Validators.required,
+      ])
+      
+
+
+    })
+  }
+
+  onSubmit(formdata: any) {
+
   }
 
 }
