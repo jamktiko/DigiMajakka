21d1938bc2180213066d95aa9e75de26d2f28462
Ranumo <aa3731@student.jamk.fi>
2022-10-26 13:26:00 +0300

    prettier muokkaus

--- client/angular-pwa/src/app/app.module.ts
+++ client/angular-pwa/src/app/app.module.ts
@@ -4,22 +4,37 @@ import {HttpClientModule} from '@angular/common/http';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import { StudentFrontpageComponent } from './student-frontpage/student-frontpage.component';
-import { TopbarComponent } from './topbar/topbar.component';
-import { JoblistsmallComponent } from './joblistsmall/joblistsmall.component';
-import { ProfilecarouselComponent } from './profilecarousel/profilecarousel.component';
-import { EditContactInfoComponent } from './edit-contact-info/edit-contact-info.component';
+import {StudentFrontpageComponent} from './student-frontpage/student-frontpage.component';
+import {TopbarComponent} from './topbar/topbar.component';
+import {JoblistsmallComponent} from './joblistsmall/joblistsmall.component';
+import {ProfilecarouselComponent} from './profilecarousel/profilecarousel.component';
+import {EditContactInfoComponent} from './edit-contact-info/edit-contact-info.component';
 import {ModalComponent} from './modal/modal.component';
-import { EditPersonalInfoComponent } from './edit-personal-info/edit-personal-info.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { AddPhotoComponent } from './add-photo/add-photo.component';
-import { ProfileComponent } from './profile/profile.component';
-
-
+import {EditPersonalInfoComponent} from './edit-personal-info/edit-personal-info.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {AddPhotoComponent} from './add-photo/add-photo.component';
+import {ProfileComponent} from './profile/profile.component';
 
 @NgModule({
-	declarations: [AppComponent, StudentFrontpageComponent, TopbarComponent, JoblistsmallComponent, ProfilecarouselComponent, ProfileComponent, ModalComponent, EditContactInfoComponent, EditPersonalInfoComponent, AddPhotoComponent],
-	imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule, ReactiveFormsModule],
+	declarations: [
+		AppComponent,
+		StudentFrontpageComponent,
+		TopbarComponent,
+		JoblistsmallComponent,
+		ProfilecarouselComponent,
+		ProfileComponent,
+		ModalComponent,
+		EditContactInfoComponent,
+		EditPersonalInfoComponent,
+		AddPhotoComponent,
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		HttpClientModule,
+		FormsModule,
+		ReactiveFormsModule,
+	],
 	providers: [],
 	bootstrap: [AppComponent],
 })
