65b04bb7c664ab1e06e61bf944d561ea59d3b20b
Tommi <n5008@student.jamk.figit config --global user.email n5008@student.jamk.figit config --global user.email n5008@student.jamk.fi>
2022-10-28 13:11:27 +0300

    lomakkeiden validointeja paranneltu

--- client/angular-pwa/src/app/edit-attachments/edit-attachments.component.ts
+++ client/angular-pwa/src/app/edit-attachments/edit-attachments.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-attachments',
@@ -7,9 +8,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditAttachmentsComponent implements OnInit {
 
+  cv: string = '';
+  portfolio: string = '';
+  github: string = '';
+
+  detailForm!: FormGroup;
+
+
   constructor() { }
 
+  onSubmit(formdata:any) {
+
+  }
+
+  changeVisibility() {
+  }
+
   ngOnInit(): void {
+    this.detailForm = new FormGroup({
+      // Email is a required field, it must be atleast 4 characters long and can be 45 characters long at maximum
+      cv: new FormControl(this.cv, [
+        Validators.minLength(4),
+        Validators.maxLength(200)
+      ]),
+      portfolio: new FormControl(this.portfolio, [
+        Validators.minLength(4),
+        Validators.maxLength(200)
+      ]),
+      github: new FormControl(this.github, [
+        Validators.minLength(4),
+        Validators.maxLength(200),
+      ]),
+    })
   }
 
 }
--- client/angular-pwa/src/app/job.service.spec.ts
+++ client/angular-pwa/src/app/job.service.spec.ts
@@ -13,4 +13,8 @@ describe('JobService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+ // it('should get data from database', () => {
+   // expect(service.getJobs()).toContain('orja');
+ // })
 });
