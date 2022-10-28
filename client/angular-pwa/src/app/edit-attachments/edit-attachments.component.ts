3b970873bdbdd3c7c1a3a524d33af08cfe55664f
Tommi <n5008@student.jamk.figit config --global user.email n5008@student.jamk.figit config --global user.email n5008@student.jamk.fi>
2022-10-28 14:53:47 +0300

    muokattu lomakkeiden validointeja järkevämmiksi

--- client/angular-pwa/src/app/edit-attachments/edit-attachments.component.ts
+++ client/angular-pwa/src/app/edit-attachments/edit-attachments.component.ts
@@ -1,45 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import {Component, OnInit} from '@angular/core';
+import {FormGroup, FormControl, Validators} from '@angular/forms';
 
 @Component({
-  selector: 'app-edit-attachments',
-  templateUrl: './edit-attachments.component.html',
-  styleUrls: ['./edit-attachments.component.css']
+	selector: 'app-edit-attachments',
+	templateUrl: './edit-attachments.component.html',
+	styleUrls: ['./edit-attachments.component.css'],
 })
 export class EditAttachmentsComponent implements OnInit {
+	info = {
+		cv: '',
+		portfolio: '',
+		github: '',
+	};
 
-  cv: string = '';
-  portfolio: string = '';
-  github: string = '';
+	detailForm!: FormGroup;
 
-  detailForm!: FormGroup;
+	constructor() {}
 
+	onSubmit(formdata: any) {}
 
-  constructor() { }
+	changeVisibility() {}
 
-  onSubmit(formdata:any) {
-
-  }
-
-  changeVisibility() {
-  }
-
-  ngOnInit(): void {
-    this.detailForm = new FormGroup({
-      // Email is a required field, it must be atleast 4 characters long and can be 45 characters long at maximum
-      cv: new FormControl(this.cv, [
-        Validators.minLength(4),
-        Validators.maxLength(200)
-      ]),
-      portfolio: new FormControl(this.portfolio, [
-        Validators.minLength(4),
-        Validators.maxLength(200)
-      ]),
-      github: new FormControl(this.github, [
-        Validators.minLength(4),
-        Validators.maxLength(200),
-      ]),
-    })
-  }
-
+	ngOnInit(): void {}
 }
