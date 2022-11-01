9eaa81b6ea53ba04fe1a15d960cc93b22c26beb8
Ranumo <aa3731@student.jamk.fi>
2022-11-01 14:05:10 +0200

    multidimensional modal component

--- client/angular-pwa/src/app/modal/modal.component.ts
+++ client/angular-pwa/src/app/modal/modal.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-modal',
-  templateUrl: './modal.component.html',
-  styleUrls: ['./modal.component.css']
+	selector: 'app-modal',
+	templateUrl: './modal.component.html',
+	styleUrls: ['./modal.component.css'],
 })
 export class ModalComponent implements OnInit {
+	//Depending on number 0-5 modal will not show or show
+	//specific message.
+	isNumber: number = 5;
+	constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+	ngOnInit(): void {}
 }
