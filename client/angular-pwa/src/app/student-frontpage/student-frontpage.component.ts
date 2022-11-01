7fb2fe5a11e97d46829d1495fe356eea94fb5c3b
Tommi <n5008@student.jamk.figit config --global user.email n5008@student.jamk.figit config --global user.email n5008@student.jamk.fi>
2022-11-01 14:50:45 +0200

    profiilikarusellin ja pienen työpaikkalistan tietokantahaku korjattu

--- client/angular-pwa/src/app/student-frontpage/student-frontpage.component.ts
+++ client/angular-pwa/src/app/student-frontpage/student-frontpage.component.ts
@@ -1,34 +1,31 @@
-import { Component, OnInit } from '@angular/core';
-import { LoginService } from '../login.service';
+import {Component, OnInit} from '@angular/core';
+import {LoginService} from '../login.service';
 
 @Component({
-  selector: 'app-student-frontpage',
-  templateUrl: './student-frontpage.component.html',
-  styleUrls: ['./student-frontpage.component.css']
+	selector: 'app-student-frontpage',
+	templateUrl: './student-frontpage.component.html',
+	styleUrls: ['./student-frontpage.component.css'],
 })
 export class StudentFrontpageComponent implements OnInit {
+	constructor(private loginService: LoginService) {}
 
-  constructor(private loginService: LoginService) { }
+	// Declarations for logged-status and currently logged in user
+	logged = this.loginService.logged;
+	loggedUser = this.loginService.loggedUser;
 
-  // Declarations for logged-status and currently logged in user
-  logged = this.loginService.logged;
-  loggedUser = this.loginService.loggedUser;
+	ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
+	// Placeholder method to set logged in status
+	login() {
+		this.loginService.login('orja@gmail.com');
+		this.logged = this.loginService.logged;
+		this.loggedUser = this.loginService.loggedUser;
+	}
 
-  // Placeholder method to set logged in status
-  login() {
-    this.loginService.login('testi');
-    this.logged = this.loginService.logged;
-    this.loggedUser = this.loginService.loggedUser;
-  }
-
-  // Placeholder method to logout
-  logout() {
-    this.loginService.logout();
-    this.logged = this.loginService.logged;
-    this.loggedUser = this.loginService.loggedUser;
-  }
-
+	// Placeholder method to logout
+	logout() {
+		this.loginService.logout();
+		this.logged = this.loginService.logged;
+		this.loggedUser = this.loginService.loggedUser;
+	}
 }
