1006d4a16f4976c7684a79039892725cdfa48ac3
Ranumo <aa3731@student.jamk.fi>
2022-11-08 16:53:52 +0200

    6 multidimensional modal + employer component hommia

--- client/angular-pwa/src/app/app-routing.module.ts
+++ client/angular-pwa/src/app/app-routing.module.ts
@@ -1,14 +1,16 @@
 /* eslint-disable @typescript-eslint/consistent-type-imports */
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import { ProfileComponent } from './profile/profile.component';
-import { StudentFrontpageComponent } from './student-frontpage/student-frontpage.component';
+import {ProfileComponent} from './profile/profile.component';
+import {StudentFrontpageComponent} from './student-frontpage/student-frontpage.component';
+import {EmployerComponent} from './employer/employer.component';
 
 const routes: Routes = [
 	// Routes. Path is the url they are accessed by (localhost:4200/<path>), and component declares
 	// which component to show in that url.
 	{path: 'student', component: StudentFrontpageComponent},
 	{path: 'profile', component: ProfileComponent},
+	{path: 'employer', component: EmployerComponent},
 ];
 
 @NgModule({
