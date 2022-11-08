3a1312efadd4ca15c513c21608d0cfa0e746425e
Tommi <n5008@student.jamk.figit config --global user.email n5008@student.jamk.figit config --global user.email n5008@student.jamk.fi>
2022-11-08 15:03:51 +0200

    Osaamisen muokkaulomake lisätty profiilin muokkausnäkymään

--- client/angular-pwa/src/app/edit-skills/edit-skills.component.ts
+++ client/angular-pwa/src/app/edit-skills/edit-skills.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-edit-skills',
-  templateUrl: './edit-skills.component.html',
-  styleUrls: ['./edit-skills.component.css']
+	selector: 'app-edit-skills',
+	templateUrl: './edit-skills.component.html',
+	styleUrls: ['./edit-skills.component.css'],
 })
 export class EditSkillsComponent implements OnInit {
+	constructor() {}
 
-  constructor() { }
+	@Input() loggedProfile: any;
 
-  ngOnInit(): void {
-  }
+	skills: any;
 
+	ngOnInit(): void {}
+
+	changeVisibility() {}
+
+	onSubmit(formadata: any) {}
 }
