daf3269fbe679bc662fd2075eff8562072839df8
Ranumo <aa3731@student.jamk.fi>
2022-11-15 17:44:19 +0200

    job-announcement

--- client/angular-pwa/src/app/job-announcement/job-announcement.component.ts
+++ client/angular-pwa/src/app/job-announcement/job-announcement.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-job-announcement',
-  templateUrl: './job-announcement.component.html',
-  styleUrls: ['./job-announcement.component.css']
+	selector: 'app-job-announcement',
+	templateUrl: './job-announcement.component.html',
+	styleUrls: ['./job-announcement.component.css'],
 })
 export class JobAnnouncementComponent implements OnInit {
+	employerInfo = 'Esimerkki';
+	payInfo = 'Placeholder';
+	startInfo = 'ETC';
+	contactInfo = 'ETC';
+	emailInfo = 'ETC';
+	phoneInfo = 'ETC';
 
-  constructor() { }
+	info = {
+		firstname: '',
+		lastname: '',
+		email: '',
+		phone: '',
+		message: '',
+	};
+	constructor() {}
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {}
 
+	// Method that has the functionality for submitting the form
+	onSubmit(formdata: any) {
+		// ADD FUNCTIONALITY
+		this.info.firstname = formdata.firstname;
+		console.log(this.info.firstname);
+	}
 }
