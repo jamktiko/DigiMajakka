c9280ee20531e0f26f4a0a4c2b305705464dbdfe
Tommi <n5008@student.jamk.figit config --global user.email n5008@student.jamk.figit config --global user.email n5008@student.jamk.fi>
2022-11-30 15:15:59 +0200

    Lisätty kommentit salasanan palautuskomponenttiin

--- client/angular-pwa/src/app/recover-password/recover-password.component.ts
+++ client/angular-pwa/src/app/recover-password/recover-password.component.ts
@@ -15,29 +15,35 @@ export class RecoverPasswordComponent implements OnInit {
 		private stateservice: StateManagementService
 	) {}
 
+	// Variable to hold form-value
 	email: string = '';
 
+	// Variable to see if there was an error while sending the resetcode
 	resetError: boolean = false;
 
 	ngOnInit(): void {}
 
-	// Method that hides or displays the form
-	changeVisibility() {
-		this.stateservice.toggleLoginFormVisibility();
-	}
-
+	// Method to send the resetcode to the users email
 	onSubmit(formData: any) {
 		this.loginservice.resetPassword(formData.email).subscribe(
 			() => {
+				// Functionality when the sending is successful
 				console.log('Sent');
 			},
 			(Error) => {
+				// Error handling
 				console.log('Error sending the reset code.');
 				this.resetError = true;
 			}
 		);
 	}
 
+	// Method that hides or displays the form
+	changeVisibility() {
+		this.stateservice.toggleLoginFormVisibility();
+	}
+
+	// Method to redirect to registration form
 	showRegisterForm() {
 		this.changeVisibility();
 		this.stateservice.toggleRegisterFormVisibility();
